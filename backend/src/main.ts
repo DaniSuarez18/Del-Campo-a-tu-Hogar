33b79978f699fbd3c5fc7a7a1faa1991c3698a93
DaniSuarez18 <danielsuarezsandboard@gmail.com>
2026-09-18 12:00:44 -0500

    feat(auth): conexion exitosa de registro de usuarios entre react y nestjs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // Habilitar CORS para permitir conexiones desde React
+  app.enableCors();
+
+  await app.listen(3000);
 }
-void bootstrap();
+bootstrap();
